98876dae94718151ea6e2b34ec7e68e62300841a
shiym <shi@localhost.localdomain>
2013-03-27 20:11:13 +0800

    add sockintf.h

---
+++ network/sock/sockintf.h
@@ -0,0 +1,54 @@
+#ifndef _SYM_SOCKINTF_H
+#define _SYM_SOCKINTF_H
+
+
+namespace sym
+{
+
+class ISocketAddress
+{
+public:
+	virtual int GetType() = 0;
+};
+
+class IInetSocketAddress : public ISocketAddress
+{
+public:
+};
+
+class ILocalSocketAddress : public ISocketAddress
+{
+
+};
+
+class ISocket
+{
+public:
+	virtual int GetHandle() const = 0;
+	virtual int Bind(const ISocketAddress& addr) = 0;
+	virtual int Connect(const ISocketAddress& raddr) = 0;
+	virtual int Close() = 0;
+};
+
+class IServerSocket
+{
+public:
+	virtual int GetHandle() const = 0;
+	virtual int Bind(const ISocketAddress& addr) = 0;
+	virtual int Listen(int backlog) = 0;
+	virtual int Accept() = 0;
+	virtual int Close() = 0;
+};
+
+class CInetServerSocket : public IServerSocket
+{
+public:
+	
+};
+
+
+} // end of namespace sym
+
+
+#endif // _SYM_SOCKINTF_H
+
