33f3622baf93b20373eb1f7d88067984937ee6e1
shiym <shi@localhost.localdomain>
2013-03-28 19:30:28 +0800

    implementation classes declared in inetsock.cpp

--- network/sock/inetsock.cpp
+++ network/sock/inetsock.cpp
@@ -1,19 +1,81 @@
-#include <sockintf.h>
+#include <stdint.h>
+#include <string>
+#include "sockintf.h"
 
-
+////////////////////////////////////////////
+// Declaration of classes
+//
 
 namespace sym
 {
 
-class CInetServerSocket : public IInetServerSocket
+class CInetSocketAddress : public IInetSocketAddress
 {
 public:
-
+	uint32_t GetIPAddress() const;
+	int      GetIPAddressStr(std::string& ipaddr) const;
+	uint16_t GetPort() const;
+	int      SetIPAddress(uint32_t addr);
+	int      SetIPAddressStr(const std::string& ipaddr);
+	int      SetPort(uint16_t port);
 };
 
+class CLocalSocketAddress : public ILocalSocketAddress
+{
+public:
+	int  GetAddressPath(std::string& path) const;
+	int  SetAddressPath(const std::string& path);
+};
 
+class CSocket : public ISocket
+{
+public:
+	int GetHandle() const;
+	int Close();
+protected:
+	int m_hsock;
+};
 
+class CServerSocket : public CSocket, public IServerSocket
+{
+public:
+	int Listen(int backlog);
+};
 
+class CTcpSocket : public CSocket, public ITcpSocket
+{
+public:
+	int Bind(const IInetSocketAddress& laddr);
+	int Connect(const IInetSocketAddress& raddr);
+};
 
+class CTcpServerSocket : public CServerSocket, public ITcpServerSocket
+{
+public:
+	int Accept(ITcpSocket& sock);
+	int Bind(const IInetSocketAddress& laddr);
+};
+
+class CUdpSocket : public CSocket, public IUdpSocket
+{
+public:
+	int Bind(const IInetSocketAddress& laddr);
+};
+
+class CLocalSocket : public CSocket, public ILocalSocket
+{
+public:
+	int Bind(const ILocalSocketAddress& laddr);
+	int Connect(const ILocalSocketAddress& raddr);
+};
+
+class CLocalServerSocket : public CServerSocket, public ILocalServerSocket
+{
+public:
+	int Accept(ILocalSocket& rsock);
+	int Bind(const ILocalSocketAddress& laddr);
+};
 
 } // end of namespace sym
+
+
--- network/sock/sockintf.h
+++ network/sock/sockintf.h
@@ -4,41 +4,71 @@
 namespace sym
 {
 
-class ISocketAddress
+class IInetSocketAddress 
 {
 public:
-	virtual int GetType() = 0;
+	virtual uint32_t GetIPAddress() const         = 0;
+	virtual int      GetIPAddressStr(std::string& ipaddr) const = 0;
+  	virtual uint16_t GetPort() const              = 0;
+	virtual int      SetIPAddress(uint32_t addr);
+	virtual int      SetIPAddressStr(const std::string& ipaddr) = 0;
+	virtual int      SetPort(uint16_t port) = 0;
 };
 
-class IInetSocketAddress : public ISocketAddress
+class ILocalSocketAddress 
 {
 public:
-};
-
-class ILocalSocketAddress : public ISocketAddress
-{
-
+	virtual int  GetAddressPath(std::string& path) const = 0;
+	virtual int  SetAddressPath(const std::string& path) = 0;
 };
 
 class ISocket
 {
 public:
 	virtual int GetHandle() const = 0;
-	virtual int Bind(const ISocketAddress& addr) = 0;
-	virtual int Connect(const ISocketAddress& raddr) = 0;
 	virtual int Close() = 0;
 };
 
-class IServerSocket
+class IServerSocket : public ISocket
 {
 public:
-	virtual int GetHandle() const = 0;
-	virtual int Bind(const ISocketAddress& addr) = 0;
 	virtual int Listen(int backlog) = 0;
-	virtual int Accept() = 0;
-	virtual int Close() = 0;
 };
 
+class ITcpSocket : public ISocket
+{
+public:
+	virtual int Bind(const IInetSocketAddress& laddr) = 0;
+	virtual int Connect(const IInetSocketAddress& raddr) = 0;
+};
+
+class ITcpServerSocket : public IServerSocket
+{
+public:
+	virtual int Accept(ITcpSocket& rsock) = 0;
+	virtual int Bind(const IInetSocketAddress& laddr) = 0;
+};
+
+class IUdpSocket : public ISocket
+{
+public:
+	virtual int Bind(const IInetSocketAddress& laddr) = 0;
+	virtual int Connect(const IInetSocketAddress& raddr) = 0;
+};
+
+class ILocalSocket : public ISocket
+{
+public:	
+	virtual int Bind(const ILocalSocketAddress& laddr) = 0;
+	virtual int Connect(const ILocalSocketAddress& raddr) = 0;
+};
+
+class ILocalServerSocket : public IServerSocket
+{
+public:
+	virtual int Accept(ILocalSocket& rsock) = 0;
+	virtual int Bind(const ILocalSocketAddress& laddr) = 0;
+};
 
 } // end of namespace sym
 
