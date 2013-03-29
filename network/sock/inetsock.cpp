882292b79a1f835cf77b7e30280016286c035dbe
shiym <shi@localhost.localdomain>
2013-03-30 02:24:28 +0800

    impl inet tcp/udp socket, modify the makefile

--- network/sock/inetsock.cpp
+++ network/sock/inetsock.cpp
@@ -13,7 +13,7 @@
 namespace sym
 {
 
-class CInetSocketAddress : public IInetSocketAddress
+class CInetSocketAddress : virtual public IInetSocketAddress
 {
 public: 
 	//---- Inherited from IInetSocketAddress
@@ -28,66 +28,85 @@ public:
 public:
 	// Local member functions
 	CInetSocketAddress();
-	virtual ~CInetSocketAddress() {};  // keep empty
 
 public:
 	//---- Public variables
 	struct sockaddr_in m_addr;
 };
 
-class CLocalSocketAddress : public ILocalSocketAddress
+class CLocalSocketAddress : virtual public ILocalSocketAddress
 {
 public:
 	int  GetAddressPath(std::string& path) const;
 	int  SetAddressPath(const std::string& path);
 };
 
-class CSocket : public ISocket
+class CSocket : virtual public ISocket
 {
 public:
-	int GetHandle() const;
-	int Close();
+	//-- Inherited for ISocket
+	virtual int GetHandle() const;
+	virtual int Close();
+
+public:
+	//-- Local memver functions
+	CSocket(int af, int type);
+	CSocket(int sock);
+
 protected:
-	int m_hsock;
+	int m_hsock;   // socket file descriptor
 };
 
-class CServerSocket : public CSocket, public IServerSocket
+class CServerSocket : public CSocket, virtual public IServerSocket
 {
-public:
+public: //-- Inheritted from IServerSocket
 	int Listen(int backlog);
+
+public:
+	CServerSocket(int af);
 };
 
-class CTcpSocket : public CSocket, public ITcpSocket
+class CTcpSocket : public CSocket, virtual public ITcpSocket
 {
-public:
+public: //-- Inherited from ITcpSocket
 	int Bind(const IInetSocketAddress& laddr);
 	int Connect(const IInetSocketAddress& raddr);
+
+public:
+	CTcpSocket();
+	CTcpSocket(int hsock);
 };
 
-class CTcpServerSocket : public CServerSocket, public ITcpServerSocket
+class CTcpServerSocket : public CServerSocket, virtual public ITcpServerSocket
 {
 public:
-	int Accept(ITcpSocket& sock);
+	int Accept(IInetSocketAddress& raddr, TObjectHolder<ITcpSocket>& sock);
 	int Bind(const IInetSocketAddress& laddr);
+
+public:
+	CTcpServerSocket();
 };
 
-class CUdpSocket : public CSocket, public IUdpSocket
+class CUdpSocket : public CSocket, virtual public IUdpSocket
 {
 public:
 	int Bind(const IInetSocketAddress& laddr);
+
+public:
+	CUdpSocket();
 };
 
-class CLocalSocket : public CSocket, public ILocalSocket
+class CLocalSocket : public CSocket, virtual public ILocalSocket
 {
 public:
 	int Bind(const ILocalSocketAddress& laddr);
 	int Connect(const ILocalSocketAddress& raddr);
 };
 
-class CLocalServerSocket : public CServerSocket, public ILocalServerSocket
+class CLocalServerSocket : public CServerSocket, virtual public ILocalServerSocket
 {
 public:
-	int Accept(ILocalSocket& rsock);
+	int Accept(ILocalSocketAddress& raddr, TObjectHolder<ILocalSocket>& rsock);
 	int Bind(const ILocalSocketAddress& laddr);
 };
 
@@ -99,6 +118,9 @@ public:
 
 namespace sym
 {
+
+/////////////////////////////////////////////
+// Implementation of class CInetSocketAddress
 
 uint32_t CInetSocketAddress::GetHostAddress() const
 {
@@ -133,6 +155,110 @@ int CInetSocketAddress::SetPort(uint16_t port)
 {
 	m_addr.sin_port = ::htons(port);
 	return 0;
+}
+
+////////////////////////////////////////////////
+// implementation of class CSocket
+
+CSocket::CSocket(int af, int type) : m_hsock(-1)
+{
+	m_hsock = ::socket(af, type, 0);
+	return ;
+}
+
+CSocket::CSocket(int sock) : m_hsock(sock) { }
+
+int CSocket::GetHandle() const 
+{
+	return m_hsock;
+}
+
+int CSocket::Close()
+{
+	if ( m_hsock >= 0) ::close(m_hsock);
+	m_hsock = -1;
+	return 0;
+}
+
+/////////////////////////////////////////
+// Implementation of class CServerSocket
+
+CServerSocket::CServerSocket(int af) : CSocket(af, SOCK_STREAM){ }
+
+int CServerSocket::Listen(int backlog)
+{
+	new CSocket(4);
+	return ::listen(m_hsock, backlog);
+}
+
+////////////////////////////////////////
+// Implementation of class CTcpSocket
+
+CTcpSocket::CTcpSocket() : CSocket(PF_INET, SOCK_STREAM) {}
+
+CTcpSocket::CTcpSocket(int hsock) : CSocket(hsock) {}
+
+int CTcpSocket::Bind(const IInetSocketAddress& laddr)
+{
+	struct sockaddr_in addr;
+	addr.sin_addr.s_addr = ::htonl(laddr.GetHostAddress());
+	addr.sin_port = ::htons(laddr.GetPort());
+
+	return ::bind(m_hsock, (struct sockaddr *)&addr, (socklen_t)sizeof(addr));
+}
+
+int CTcpSocket::Connect(const IInetSocketAddress& raddr)
+{
+	struct sockaddr_in addr;
+	addr.sin_addr.s_addr = ::htonl(raddr.GetHostAddress());
+	addr.sin_port = ::htons(raddr.GetPort());
+
+	return ::connect(m_hsock, (struct sockaddr*)&addr, (socklen_t)sizeof(addr)); 
+}
+
+//////////////////////////////////////////
+// Implementation of class CTcpServerSocket
+
+CTcpServerSocket::CTcpServerSocket() : CServerSocket(PF_INET) {}
+
+int CTcpServerSocket::Accept(IInetSocketAddress& raddr, 
+		                     TObjectHolder<ITcpSocket>& sockHolder)
+{
+	struct    sockaddr_in addr;
+	socklen_t len = sizeof(addr);
+	int s = ::accept(m_hsock, (struct sockaddr *)&addr, &len);
+	if ( s >= 0) {
+		ITcpSocket * psock = sockHolder.Alloc(sizeof(CTcpSocket));
+		psock = (ITcpSocket *)new(psock) CTcpSocket(s);
+		raddr.SetHostAddress(::ntohl(addr.sin_addr.s_addr));
+		raddr.SetPort(::ntohs(addr.sin_port));
+		return 0;
+	}  else {
+		return s;
+	}
+}
+
+int CTcpServerSocket::Bind(const IInetSocketAddress& laddr)
+{
+	struct sockaddr_in addr;
+	addr.sin_addr.s_addr = ::htonl(laddr.GetHostAddress());
+	addr.sin_port = ::htons(laddr.GetPort());
+
+	return ::bind(m_hsock, (struct sockaddr*)&addr, sizeof(addr));
+}
+
+/////////////////////////////////////////////
+// Implementation of class CUdpSocket
+
+CUdpSocket::CUdpSocket() : CSocket(PF_INET, SOCK_DGRAM){}
+
+int CUdpSocket::Bind(const IInetSocketAddress& laddr) 
+{
+	struct sockaddr_in addr;
+	addr.sin_addr.s_addr = ::htonl(laddr.GetHostAddress());
+	addr.sin_port = ::htons(laddr.GetPort());
+
+	return ::bind(m_hsock, (struct sockaddr*)&addr, sizeof(addr));
 }
 
 } // end of namespace sym
--- network/sock/sockintf.h
+++ network/sock/sockintf.h
@@ -4,6 +4,62 @@
 namespace sym
 {
 
+class TDefaultAllocator
+{
+public:
+	template<class T>
+	T*   Alloc() { return new T();}
+
+	template<class T>
+	void Free(T* p) { delete p;}
+};
+
+template<class T>
+class TObjectHolder
+{
+public:
+	typedef T OBJTYPE, *POBJTYPE;
+	
+
+private:
+	POBJTYPE m_pobj;
+
+public:	
+	TObjectHolder() : m_pobj(0) {}
+	TObjectHolder(POBJTYPE pobj) : m_pobj(pobj) {}
+	~TObjectHolder() {
+		if ( m_pobj ) Free(m_pobj);
+		return ;
+	}
+
+	POBJTYPE Get() const { return m_pobj; }
+	
+	void Put(POBJTYPE pobj)
+	{ 
+		if ( m_pobj ) Free(pobj);
+		m_pobj = pobj; 
+	}
+
+	POBJTYPE Pop() 
+	{
+		POBJTYPE p = m_pobj;
+		m_pobj = 0;
+		return p;
+	}
+
+	POBJTYPE Alloc(size_t size) 
+	{
+		if ( m_pobj ) Free();	
+		m_pobj =  (POBJTYPE)::operator new(size); 
+		return m_pobj;
+	}
+	void  Free() { delete m_pobj; m_pobj = 0; }
+
+private:
+	TObjectHolder(const TObjectHolder<T>&);
+	TObjectHolder<T>& operator=(const TObjectHolder<T>&);
+};
+
 class IInetSocketAddress 
 {
 public:
@@ -29,44 +85,46 @@ public:
 	virtual int Close() = 0;
 };
 
-class IServerSocket : public ISocket
+class IServerSocket : virtual public ISocket
 {
 public:
 	virtual int Listen(int backlog) = 0;
 };
 
-class ITcpSocket : public ISocket
+class ITcpSocket : virtual public ISocket
 {
 public:
 	virtual int Bind(const IInetSocketAddress& laddr) = 0;
 	virtual int Connect(const IInetSocketAddress& raddr) = 0;
 };
 
-class ITcpServerSocket : public IServerSocket
+class ITcpServerSocket : virtual public IServerSocket
 {
 public:
-	virtual int Accept(ITcpSocket& rsock) = 0;
+	virtual int Accept(IInetSocketAddress& raddr, 
+			           TObjectHolder<ITcpSocket>& rsock) = 0;
 	virtual int Bind(const IInetSocketAddress& laddr) = 0;
 };
 
-class IUdpSocket : public ISocket
+class IUdpSocket : virtual public ISocket
 {
 public:
 	virtual int Bind(const IInetSocketAddress& laddr) = 0;
 	virtual int Connect(const IInetSocketAddress& raddr) = 0;
 };
 
-class ILocalSocket : public ISocket
+class ILocalSocket : virtual public ISocket
 {
 public:	
 	virtual int Bind(const ILocalSocketAddress& laddr) = 0;
 	virtual int Connect(const ILocalSocketAddress& raddr) = 0;
 };
 
-class ILocalServerSocket : public IServerSocket
+class ILocalServerSocket : virtual public IServerSocket
 {
 public:
-	virtual int Accept(ILocalSocket& rsock) = 0;
+	virtual int Accept(ILocalSocketAddress& raddr, 
+			           TObjectHolder<ILocalSocket>& rsock) = 0;
 	virtual int Bind(const ILocalSocketAddress& laddr) = 0;
 };
 
