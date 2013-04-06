bd259a94d6597b233e77179b6fda6c818c3b3f38
shi yimin <shi@xbian>
2013-04-06 02:55:16 +0000

    add localchannel.h, move declaration of localchannel to localchannel.h

--- network/sock/localchannel.cpp
+++ network/sock/localchannel.cpp
@@ -3,47 +3,7 @@
 #include <sys/un.h>
 #include <string>
 #include <errno.h>
-
-///////////////////////////////////////////
-// Declaration of classes
-
-namespace sym
-{
-
-namespace detail
-{
-	class CLocalChannelImpl;
-} // end of namespace detail
-
-
-class CLocalChannel
-{
-public:
-	CLocalChannel();
-	CLocalChannel(const CLocalChannel& ch);
-	~CLocalChannel();
-
-	std::string GetLocalAddr() const;
-	std::string GetRemoteAddr() const;
-	std::string GetRecvAddr()  const;
-	
-	int Bind(const std::string& path);
-	int Connect(const std::string& path);
-	int Close();
-
-	int Send(const char *buf, size_t size, int flags = 0);
-	int Recv(char *buf, size_t size, int flags = 0); 
-
-private:
-	CLocalChannel& operator=(const CLocalChannel& ch); // no impl
-
-private:
-	detail::CLocalChannelImpl * m_pImpl;
-};
-
-
-} // end of namespace sym
-
+#include "localchannel.h"
 
 ////////////////////////////////////////////////
 // Implementation of classes
---
+++ network/sock/localchannel.h
@@ -0,0 +1,46 @@
+#ifndef _SYM_LOCALCHANNEL_H
+#define _SYM_LOCALCHANNEL_H
+
+///////////////////////////////////////////
+// Declaration of classes
+
+namespace sym
+{
+
+namespace detail
+{
+	class CLocalChannelImpl;
+} // end of namespace detail
+
+
+class CLocalChannel
+{
+public:
+	CLocalChannel();
+	CLocalChannel(const CLocalChannel& ch);
+	~CLocalChannel();
+
+	std::string GetLocalAddr() const;
+	std::string GetRemoteAddr() const;
+	std::string GetRecvAddr()  const;
+	
+	int Bind(const std::string& path);
+	int Connect(const std::string& path);
+	int Close();
+
+	int Send(const char *buf, size_t size, int flags = 0);
+	int Recv(char *buf, size_t size, int flags = 0); 
+
+private:
+	CLocalChannel& operator=(const CLocalChannel& ch); // no impl
+
+private:
+	detail::CLocalChannelImpl * m_pImpl;
+};
+
+
+} // end of namespace sym
+
+
+#endif // _SYM_LOCALCHANNEL
+
