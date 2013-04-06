4f08111ad7affe97c060bbd219b0e544776e1735
shi yimin <shi@xbian>
2013-04-06 03:03:46 +0000

    add common socket include file to sockdef.h

---
+++ network/sock/sockdef.h
@@ -0,0 +1,17 @@
+#ifndef _SYM_SOCKDEF_H
+#define _SYM_SOCKDEF_H
+
+#include <stdint.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#include <sys/un.h>
+#include <string>
+#include <errno.h>
+
+
+#endif // _SYM_SOCKDEF_H
+
+
